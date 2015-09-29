5d0e91978b2282f823dc9983f85d9ad500ed507c
Daniel Eberle <eberle@insideguidance.com>
2015-09-29 18:23:48 +0200

    Comment out RKISO8601DateFormatter in RKObjectMapping
    Fix for issue issue #1683. See:
    https://github.com/RestKit/RestKit/issues/1683

--- Code/ObjectMapping/RKObjectMapping.m
+++ Code/ObjectMapping/RKObjectMapping.m
@@ -145,8 +145,8 @@ static RKSourceToDesinationKeyTransformationBlock defaultSourceToDestinationKeyT
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         // Add an ISO8601DateFormatter to the transformation stack for backwards compatibility
-        RKISO8601DateFormatter *dateFormatter = [RKISO8601DateFormatter defaultISO8601DateFormatter];
-        [[RKValueTransformer defaultValueTransformer] insertValueTransformer:dateFormatter atIndex:0];
+//        RKISO8601DateFormatter *dateFormatter = [RKISO8601DateFormatter defaultISO8601DateFormatter];
+//        [[RKValueTransformer defaultValueTransformer] insertValueTransformer:dateFormatter atIndex:0];
     });
 }
 
